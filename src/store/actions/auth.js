6d65448505ce3ad616d0c0e9f528a19b5c09dda8
yuriyberezskyy <bereza95@gmail.com>
2020-06-03 15:50:30 -0400

    added token and personId to the store"

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData: authData,
+    idToken: token,
+    userId: userId,
   };
 };
 
@@ -41,7 +42,7 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then((res) => {
         console.log(res.data);
-        dispatch(authSuccess(res));
+        dispatch(authSuccess(res.data.idToken, res.data.localId));
       })
       .catch((error) => {
         console.log(error);
